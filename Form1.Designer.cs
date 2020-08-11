63454bb99e019809fcf574432467cd8f8454f948
なりかくん <narikakun@gmail.com>
2020-08-11 10:43:54 +0900

    upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace QuakeInfo_Lite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(6, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "タイトル";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "[震源]";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(12, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "[深 さ]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(12, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "[規模]";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(189, 103);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(12, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 16);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "[時間]";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(12, 91);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 16);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "[震度]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 113);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DataBindings.Add(new System.Windows.Forms.Binding("Location", global::QuakeInfo_Lite.Properties.Settings.Default, "Location", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = global::QuakeInfo_Lite.Properties.Settings.Default.Location;
+            this.MaximumSize = new System.Drawing.Size(400, 152);
+            this.MinimumSize = new System.Drawing.Size(400, 152);
+            this.Name = "Form1";
+            this.Text = "QuakeInfo_Lite";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuakeInfo_Lite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Check();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Check();
+        }
+
+        string last_eventid = "0";
+        string last_datetime = "0";
+        string webapiurl = "https://dev.narikakun.net/webapi/earthquake/post_data.json";
+
+        private void Check()
+        {
+            try
+            {
+                WebClient wc = new WebClient() { Encoding = Encoding.GetEncoding("UTF-8") };
+                string str = wc.DownloadString(webapiurl);
+                try
+                {
+                    var json = JObject.Parse(str);
+                    if (json["Control"]["DateTime"].Value<string>() == last_datetime) return;
+                    string cityint1 = "", cityint2 = "", cityint3 = "", cityint4 = "", cityint5 = "", cityint5plus = "", cityint6 = "", cityint6plus = "", cityint7 = "";
+                    if (last_eventid == json["Head"]["EventID"].Value<string>())
+                    {
+                        if (json["Head"]["Title"].Value<string>() != "震源に関する情報")
+                        {
+                            textBox1.Text = "";
+                        }
+                    }
+                    else
+                    {
+                        textBox1.Text = "";
+                    }
+                    if (json["Head"]["Title"].Value<string>() != "震源に関する情報")
+                    {
+                        foreach (JObject pref in json["Body"]["Intensity"]["Observation"]["Pref"])
+                        {
+                            textBox1.Text += "[" + pref["Name"].Value<string>() + "]";
+                            foreach (JObject area in pref["Area"])
+                            {
+                                if (json["Head"]["Title"].Value<string>() == "震源・震度情報")
+                                {
+                                    foreach (JObject city in area["City"])
+                                    {
+                                        if (city["MaxInt"].Value<string>() == "1") { cityint1 += city["Name"].Value<string>() + " "; }
+                                        if (city["MaxInt"].Value<string>() == "2") { cityint2 += city["Name"].Value<string>() + " "; }
+                                        if (city["MaxInt"].Value<string>() == "3") { cityint3 += city["Name"].Value<string>() + " "; }
+                                        if (city["MaxInt"].Value<string>() == "4") { cityint4 += city["Name"].Value<string>() + " "; }
+                                        if (city["MaxInt"].Value<string>() == "5-") { cityint5 += city["Name"].Value<string>() + " "; }
+                                        if (city["MaxInt"].Value<string>() == "5+") { cityint5plus += city["Name"].Value<string>() + " "; }
+                                        if (city["MaxInt"].Value<string>() == "6-") { cityint6 += city["Name"].Value<string>() + " "; }
+                                        if (city["MaxInt"].Value<string>() == "6+") { cityint6plus += city["Name"].Value<string>() + " "; }
+                                        if (city["MaxInt"].Value<string>() == "7") { cityint7 += city["Name"].Value<string>() + " "; }
+                                    }
+                                }
+                                else if (json["Head"]["Title"].Value<string>() == "震度速報")
+                                {
+                                    if (area["MaxInt"].Value<string>() == "1") { cityint1 += area["Name"].Value<string>() + " "; }
+                                    if (area["MaxInt"].Value<string>() == "2") { cityint2 += area["Name"].Value<string>() + " "; }
+                                    if (area["MaxInt"].Value<string>() == "3") { cityint3 += area["Name"].Value<string>() + " "; }
+                                    if (area["MaxInt"].Value<string>() == "4") { cityint4 += area["Name"].Value<string>() + " "; }
+                                    if (area["MaxInt"].Value<string>() == "5-") { cityint5 += area["Name"].Value<string>() + " "; }
+                                    if (area["MaxInt"].Value<string>() == "5+") { cityint5plus += area["Name"].Value<string>() + " "; }
+                                    if (area["MaxInt"].Value<string>() == "6-") { cityint6 += area["Name"].Value<string>() + " "; }
+                                    if (area["MaxInt"].Value<string>() == "6+") { cityint6plus += area["Name"].Value<string>() + " "; }
+                                    if (area["MaxInt"].Value<string>() == "7") { cityint7 += area["Name"].Value<string>() + " "; }
+                                }
+                            }
+                            if (cityint7 != "") { textBox1.Text += "震度7: " + cityint7; }
+                            if (cityint6plus != "") { textBox1.Text += "震度6強: " + cityint6plus; }
+                            if (cityint6 != "") { textBox1.Text += "震度6弱: " + cityint6; }
+                            if (cityint5plus != "") { textBox1.Text += "震度5強: " + cityint5plus; }
+                            if (cityint5 != "") { textBox1.Text += "震度5弱: " + cityint5; }
+                            if (cityint4 != "") { textBox1.Text += "震度4: " + cityint4; }
+                            if (cityint3 != "") { textBox1.Text += "震度3: " + cityint3; }
+                            if (cityint2 != "") { textBox1.Text += "震度2: " + cityint2; }
+                            if (cityint1 != "") { textBox1.Text += "震度1: " + cityint1; }
+                            cityint1 = "";
+                            cityint2 = "";
+                            cityint3 = "";
+                            cityint4 = "";
+                            cityint5 = "";
+                            cityint5plus = "";
+                            cityint6 = "";
+                            cityint6plus = "";
+                            cityint7 = "";
+                            textBox1.Text += "\r\n";
+                        }
+                    }
+                    label1.Text = json["Head"]["Title"].Value<string>();
+                    if (json["Head"]["Title"].Value<string>() == "震源・震度情報")
+                    {
+                        string d = json["Body"]["Earthquake"]["OriginTime"].Value<string>();
+                        string f = "yyyy-MM-dd HH:mm:ss";
+                        DateTime dt = DateTime.ParseExact(d, f, null);
+                        label6.Text = "[時間] " + dt.ToString("dd日hh時mm分");
+                        label7.Text = "[震度] " + json["Body"]["Intensity"]["Observation"]["MaxInt"].Value<string>().Replace("-", "弱").Replace("+", "強");
+                    }
+                    else if (json["Head"]["Title"].Value<string>() == "震度速報")
+                    {
+                        label7.Text = "[震度] " + json["Body"]["Intensity"]["Observation"]["MaxInt"].Value<string>().Replace("-", "弱").Replace("+", "強");
+                    }
+                    if (json["Head"]["Title"].Value<string>() == "震源・震度情報")
+                    {
+                        label2.Text = "[震源] " + json["Body"]["Earthquake"]["Hypocenter"]["Name"].Value<string>();
+                    }
+                    else
+                    {
+                        label2.Text = "[震源] 調査中";
+                    }
+                    if (json["Head"]["Title"].Value<string>() != "震度速報")
+                    {
+                        label3.Text = "[深さ] " + json["Body"]["Earthquake"]["Hypocenter"]["Depth"].Value<string>() + "km";
+                        label4.Text = "[規模] " + json["Body"]["Earthquake"]["Magnitude"].Value<string>();
+                    }
+                    else
+                    {
+                        label3.Text = "[深さ] 調査中";
+                        label4.Text = "[規模] 調査中";
+                    }
+                    last_eventid = json["Head"]["EventID"].Value<string>();
+                    last_datetime = json["Control"]["DateTime"].Value<string>();
+                    if (timer1.Interval != 3000)
+                    {
+                        timer1.Stop();
+                        timer1.Interval = 3000;
+                        timer1.Start();
+                        Console.WriteLine("更新時間を3秒に戻します。");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("例外処理 更新時間を10秒に変更します。");
+                    timer1.Stop();
+                    timer1.Interval = 10000;
+                    timer1.Start();
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine("ネットワークエラー 更新時間を10秒に変更します。" + ex);
+                timer1.Stop();
+                timer1.Interval = 10000;
+                timer1.Start();
+            }
+        }
+    }
+}
